7ba50f3cbc854f936bde357adf03836b8edef833
ErickMwirigi <erickmbae21@gmail.com>
2023-09-13 15:03:30 +0300

    Task

--- src/DailyData.js
+++ src/DailyData.js
@@ -35,7 +35,9 @@ function DailyData(){
       const Mkulima = WD.filter(F => F.datetime === '2023-09-12').map((shamba) =>
       {return <li key={Object.keys(WD[0])}>
            <h1>WEATHER DESC:  {shamba.weather.description}</h1>
-          
+           <h1>WIND DIRECTION: {shamba.wind_cdir_full}</h1>
+           <h1>WIND SPEED: {shamba.wind_gust_spd} KM/HR</h1>
+           <h1>SNOW: {shamba.snow}</h1>
       </li>}
       )
       
@@ -45,12 +47,13 @@ function DailyData(){
           <div>
 
              <h1 className="Head">Weather Today</h1> 
+
              <nav className="category">
-               <li onClick={handleUser}> 
-                <a> USER</a>
+               <li onClick={handleUser} className="user"> 
+                <h1>USER</h1>
                </li>
-               <li onClick={handleFarmer}> 
-                <a> FARMER</a>
+               <li onClick={handleFarmer} className="farmer"> 
+                <h1>FARMER</h1>
                </li>
              </nav>
 
