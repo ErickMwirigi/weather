57509683002f67c4debabbf4376a60e6b1c1d72d
Stephen Mbau <mbaustephen@gmail.com>
2023-09-13 17:22:40 +0300

    1st commit, login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import DailyData from './DailyData';
-
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import NavBar from "./NavBar";
 
 function App() {
   return (
-    <DailyData/>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
 
